aec4fa6ab21f726835f6a84e3c088a70ccd2ff31
SKwambua <samuelkikuvi33@gmail.com>
2022-05-31 20:23:36 +0000

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(c), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    else:
+        print("{:s}".format(chr(i)), end="")
